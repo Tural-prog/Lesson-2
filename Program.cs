cbdf24490638c80fcc572c99092f9814a810746b
Tural <turalprogramci@gmail.com>
2022-10-16 00:36:15 +0400

    Projekt initally added

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tapsiriq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i = 0 ;
+            Console.Write("Metni daxil edin:\n");
+            str=Console.ReadLine();
+            for (i=0; i < str.Count( );i++)
+            { 
+
+            }
+            Console.WriteLine("Metnin uzunlugunu :{0}\n", i);
+
+        }
+    }
+}
